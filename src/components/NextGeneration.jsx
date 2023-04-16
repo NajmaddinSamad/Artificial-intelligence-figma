23dfb5ca764f98fbd9b7f284825b1284c745765f
NajmaddinSamad <gnecmeddin@gmail.com>
2023-04-16 19:39:14 +0400

    Next generation section has created and has created responsive design

--- src/components/NextGeneration.jsx
+++ src/components/NextGeneration.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import './NextGeneration.css'
-
+import React from "react";
+import "./NextGeneration.css";
+import Button from "./Button";
+import generation from '../assets/images/generation.svg'
 const NextGeneration = () => {
   return (
-  <>
-  <section className="next-generation row">
-  <div className="col-12 col-md-6 col-lg-6">
-left
-</div>
-<div className="col-12 col-md-6 col-lg-6">
-  right
-</div>
-  </section>
+    <>
+      <section className="next-generation row">
+        <div className="left col-12 col-md-6 col-lg-6">
+          <h4>Next genaretion platform</h4>
+          <h1>Artificial intelligence & Syber security</h1>
+          <p>
+            Lorem ipsum is placeholder text commonly used in the graphic, print,
+            and publishing industries for previewing layouts and visual mockups.
+          </p>
+          <Button valueOfButton="Get Started" nameOfClass="btn btn-primary text-white button" />
+          <Button valueOfButton="Watch Video" nameOfClass="btn btn-primary text-white" />
+        </div>
+        <div className="righ col-12 col-md-6 col-lg-6">
+          <img src={generation} alt="" />
+        t</div>
+      </section>
+    </>
+  );
+};
 
-  </>
-  )
-}
-
-export default NextGeneration
+export default NextGeneration;
