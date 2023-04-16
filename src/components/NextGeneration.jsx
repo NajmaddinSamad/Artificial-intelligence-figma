cab63dfe8cfae047da69999cd26175f75cf0bc65
NajmaddinSamad <gnecmeddin@gmail.com>
2023-04-16 15:45:24 +0400

    Navbar has created

--- src/components/NextGeneration.jsx
+++ src/components/NextGeneration.jsx
@@ -6,10 +6,10 @@ const NextGeneration = () => {
   <>
   <section className="next-generation row">
   <div className="col-12 col-md-6 col-lg-6">
-
+left
 </div>
 <div className="col-12 col-md-6 col-lg-6">
-  
+  right
 </div>
   </section>
 
