f56013741163a9c65fc9e9bbc6df348e731725f7
NajmaddinSamad <gnecmeddin@gmail.com>
2023-04-19 00:21:11 +0400

    email js and loader added , artificial intelligence has finished

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import '../components/Contact.css'
 import Button from '../components/Button'
+import emailjs from '@emailjs/browser';
 const Contact = () => {
+
+    const formRef = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ws6ysqk', 'template_eidzbcn', formRef.current, 'yXHtkDqs2jmrD4p-H')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      formRef.current.reset();
+  };
+
   return (
    <>
 <div className="contact">
 <h1 className="head">Contact Us</h1>
-    <form action="#">
-    <input type="text" name="" id="" placeholder='name' />
-    <input type="text" name="" id="" placeholder='Surname' />
-    <input type="email" name="" id="" placeholder='email'/>
-    <textarea name="" id="" cols="30" rows="6" placeholder='Enter your message'></textarea>
-    <Button valueOfButton='Send' nameOfClass='btn btn-primary'/>
+    <form  ref={formRef} onSubmit={sendEmail}>
+    <input type="text" name="name" id="" placeholder='Name' />
+    <input type="email" name="email" id="" placeholder='Email'/>
+    <input type="text" name="message_title" id="" placeholder='Title' />
+    <textarea name="mesaj" id="" cols="30" rows="6" placeholder='Enter your message'></textarea>
+    <Button  type='submit' valueOfButton='Send' nameOfClass='btn btn-primary'/>
     </form>
 </div>
 
 </>
   )
-}
+} 
+  
 
 export default Contact
