d0088a1aefe87606c65abe2ba3677c10d5ed4a1e
NajmaddinSamad <gnecmeddin@gmail.com>
2023-05-12 02:22:20 +0400

    changes

--- src/components/AwardsContainer.jsx
+++ src/components/AwardsContainer.jsx
@@ -5,6 +5,7 @@ import Award from './Award'
 const AwardsContainer = () => {
   return (
     <div className='awards row '>
+      
          {
 AwardsInfo.map(comingInfo =>(
     <Award comingPhoto={comingInfo.awardsPhoto}/>
