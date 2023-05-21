95e84e25af332154314d95bfe7aac90630cc4c4e
NajmaddinSamad <gnecmeddin@gmail.com>
2023-05-21 22:47:21 +0400

    several changes i did

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
 import Partner from './Partner'
-import { Swiper, SwiperSlide } from "swiper/react";
 import Partner1 from    '../assets/images/Group 400.png'
 import Partner2 from    '../assets/images/Group 401.png'
 import Partner3 from    '../assets/images/Group 402.png'
 import Partner4 from    '../assets/images/Group 403.png'
-import "swiper/css";
-import { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {  Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import '../../src/App.css'
 
 const Partners = () => {
   return (
    <>
-   <Swiper
+  
+     <div className="swiper">
+     <Swiper
         slidesPerView={"4"}
         spaceBetween={30}
         breakpoints={{
@@ -24,20 +28,15 @@ const Partners = () => {
             spaceBetween: 40,
           },
           1024: {
-            slidesPerView: 3 ,
+            slidesPerView: 4 ,
             spaceBetween: 50,
           },
         }}
-        Autoplay ={
-         {
-          delay:500 
-         }
-        }
-       
-        modules={[Autoplay]}
-        className="mySwiper"
-      >
-        <SwiperSlide><Partner partnersLogo={Partner1} /></SwiperSlide>
+      modules={[Autoplay]}
+      autoplay={{ delay: 1000 }}
+      
+    >
+  <SwiperSlide><Partner partnersLogo={Partner1} /></SwiperSlide>
         <SwiperSlide><Partner partnersLogo={Partner2} /></SwiperSlide>
         <SwiperSlide><Partner partnersLogo={Partner3} /></SwiperSlide>
         <SwiperSlide><Partner partnersLogo={Partner4} /></SwiperSlide>
@@ -46,8 +45,8 @@ const Partners = () => {
         <SwiperSlide><Partner partnersLogo={Partner2} /></SwiperSlide>
         <SwiperSlide><Partner partnersLogo={Partner3} /></SwiperSlide>
         <SwiperSlide><Partner partnersLogo={Partner4} /></SwiperSlide>       
-       
-      </Swiper>
+    </Swiper>
+     </div>
    </>
   )
 }
